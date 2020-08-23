8eb8567ad158ccb6246e51c2a06ad67a8bae8b59
Eric Andrechek <eric@andrechek.com>
2020-08-23 15:06:56 -0400

    finalizing resume scraper

--- resume_parser.py
+++ resume_parser.py
@@ -17,94 +17,6 @@ class ResumeParser:
         self.resume = dict()
 
     def read_resume(self):
-        file_obj = open(self.resume_file, 'rb')
-        self.fo = file_obj
-        pdf_reader = PyPDF2.PdfFileReader(file_obj)
-        for page_number in range(pdf_reader.numPages):
-            page_obj = pdf_reader.getPage(page_number)
-            self.resume_text += page_obj.extractText()
-        self.resume_text = self.resume_text.replace("\n", "")
-        self.resume_text = self.resume_text.lower()
-
-    def extract_name(self):
-        nlp_text = self.nlp(self.resume_text)
-        pattern = [{'POS': 'PROPN'}, {'POS': 'PROPN'}]
-        self.matcher.add('NAME', None, pattern)
-        matches = self.matcher(nlp_text)
-        for match_id, start, end in matches:
-            span = nlp_text[start:end]
-            return span.text
-
-    def extract_phone_number(self):
-        phone = re.findall(re.compile(
-            r'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})'),
-            self.resume_text)
-        try:
-            return phone[0]
-        except:
-            return None
-
-    def extract_email(self):
-        email = re.findall("([^@|\s]+@[^@]+\.[^@|\s]+)", self.resume_text)
-        if email:
-            try:
-                return email[0].split()[0].strip(';')
-            except IndexError:
-                return None
-
-    def extract_locations(self):
-        locs = list()
-        doc = self.nlp(self.resume_text)
-        for ent in doc.ents:
-            if ent.label_ == 'GPE':
-                locs.append(ent.text)
-        locations = list()
-        for loc in locs:
-            if loc not in locations:
-                locations.append(loc)
-        return locations
-
-    def extract_degree(self):
-
-        STOPWORDS = set(stopwords.words('english'))
-
-        EDUCATION = [
-            'BE', 'B.E.', 'B.E', 'BS', 'B.S',
-            'ME', 'M.E', 'M.E.', 'MS', 'M.S',
-            'BTECH', 'B.TECH', 'M.TECH', 'MTECH',
-            'ICSE', 'ISC'
-        ]
-        nlp_text = self.nlp(self.resume_text)
-
-        # Sentence Tokenizer
-        nlp_text = [sent.string.strip() for sent in nlp_text.sents]
-
-        edu = {}
-        # Extract education degree
-        for index, text in enumerate(nlp_text):
-            for tex in text.split():
-                # Replace all special symbols
-                tex = re.sub(r'[?|$|.|!|,]', r'', tex)
-                if tex.upper() in EDUCATION and tex not in STOPWORDS:
-                    edu[tex] = text + nlp_text[index + 1]
-        try:
-            return list(edu.keys())[0]
-        except:
-            return None
-
-    def extract_skills(self):
-        orgs = list()
-        doc = self.nlp(self.resume_text)
-        for ent in doc.ents:
-            if ent.label_ == 'ORG':
-                orgs.append(ent.text)
-        organizations = list()
-        for org in orgs:
-            if org not in organizations:
-                organizations.append(org)
-        return organizations
-
-    def extract_jobs(self):
         url = "https://jobs.lever.co/parseResume"
         payload = {}
         files = [
@@ -114,31 +26,47 @@ class ResumeParser:
 
         response = requests.request("POST", url, headers=headers, data = payload, files = files)
         print(response.text)
-        try:
-            jobs = response.json()['positions']
-            positions = []
-            for job in jobs:
-                position = {}
-                position['title'] = job['title']
-                position['company'] = job['org']
-                position['description'] = job['summary']
-                try:
-                    start = "{}-{}".format(job['start']['year'], job['start']['month'])
-                    end = "{}-{}".format(job['end']['year'], job['end']['month'])
-                    position['dates'] = '{} - {}'.format(start, end)
-                except KeyError:
-                    position['dates'] = ""
-                positions.append(position)
-            return positions
-        except KeyError:
-            return {'jobs': None}
+        self.fo = response.json()
+
+    def extract_name(self):
+        return self.fo['names'][0]
+
+    def extract_phone_number(self):
+        return self.fo['phones'][0]['value']
+
+    def extract_email(self):
+        return self.fo['emails'][0]['value']
+
+    def extract_locations(self):
+        return self.fo['location']['name']
+
+    def extract_degree(self):
+        return self.fo['schools'][0]['org']
+
+    def extract_skills(self):
+        return self.fo['summary']['skills']
+
+    def extract_jobs(self):
+        jobs = self.fo['positions']
+        all_work = []
+        for job in jobs:
+            parsed = {}
+            parsed['title'] = job['title']
+            parsed['company'] = job['org']
+            parsed['description'] = job['summary']
+            try:
+                start = "{}-{}".format(job['start']['year'], job['start']['month'])
+                end = "{}-{}".format(job['end']['year'], job['end']['month'])
+                parsed['dates'] = '{} - {}'.format(start, end)
+            except KeyError:
+                parsed['dates'] = ""
+            all_work.append(parsed)
+        return all_work
 
     def extract_socials(self):
-        socials = dict()
-        linkedin = re.findall("linkedin.com/[a-z,A-Z,0-9,\-,\\,\/]*", self.resume_text)
-        github = re.findall("[https://]*github.com[a-z,A-Z,0-9,\-,\\,\/]*", self.resume_text)
-        socials = {'linkedin':linkedin[0] if len(linkedin)>0 else None,
-                   'github':github[0] if len(github)>0 else None}
+        socials = {}
+        for social in self.fo['links']:
+            socials[social['domain'].split('.')[0]] = social['url']
         return socials
 
 
